ea82c4441e4af74f03ec9106c3ff6cd1dd134a5c
alexandre deiness <alex-1602@hotmail.fr>
2017-02-22 15:34:55 +0100

    update personnage

---
+++ personnage.php
@@ -0,0 +1,66 @@
+<?php
+
+class Personnage
+{
+    private $_id
+    private $_atk
+    private $_magic
+    private $_def
+    private $_hp
+    private $_weapon
+
+    public function observer()
+    {
+
+    }
+
+    public function porte()
+    {
+
+    }
+
+    public function coffre()
+    {
+
+    }
+
+    public function attaquer()
+    {
+
+    }
+
+    public function fuir()
+    {
+
+    }
+
+    public function id()
+    {
+        return $this->_id;
+    }
+
+    public function atk()
+    {
+        return $this->_atk;
+    }
+
+    public function magic()
+    {
+        return $this->_magic;
+    }
+
+    public function def()
+    {
+        return $this->_def;
+    }
+
+    public function hp()
+    {
+        return $this->_hp;
+    }
+
+    public function weapon()
+    {
+        return $this->_weapon;
+    }
+}
